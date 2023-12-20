a6c00708642cbbcf7f085c0718d36bd5d1fd008e
Kristof Ostir <krostir@gmail.com>
2023-12-20 20:22:33 +0100

    Add my LazyVim config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set spell
+vim.opt.spell = true
+-- Set spelllang to US English and Slovenian
+vim.opt.spelllang = "en,sl"
