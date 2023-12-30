d61fdd9013f9165d60c681f57e7b926e24a89c0e
Kristof Ostir <krostir@gmail.com>
2023-12-30 21:05:15 +0100

    Disable auto-save

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,9 +1,5 @@
 -- Add Pocco81/auto-save.nvim to the list of plugins
 return {
-  "Pocco81/auto-save.nvim",
-  -- Delay saving files for some time
-  debounce_delay = 1000,
-  config = function()
-    require("auto-save").setup({})
-  end,
+  -- Auto save files
+  { "Pocco81/auto-save.nvim", enabled = false }, -- Disabled
 }
