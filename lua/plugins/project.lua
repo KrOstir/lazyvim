212e3767313cb43d95d8f54317dd22f0375add3d
Kristof Ostir <krostir@gmail.com>
2024-01-10 20:16:49 +0100

    Add project
    Project management

---
+++ lua/plugins/project.lua
@@ -0,0 +1,31 @@
+-- Neovim project plugin
+return {
+  "coffebar/neovim-project",
+  enabled = true,
+  opts = {
+    projects = { -- define project roots
+      "~/.config/*",
+      "~/AppData/Local/nvim",
+      "~/PowerShell/*",
+      "~/PowerShell/*",
+      "~/Programing/*",
+      "~/Python/*",
+    },
+  },
+  init = function()
+    -- enable saving the state of plugins in the session
+    vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+  end,
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+    { "Shatur/neovim-session-manager" },
+  },
+  lazy = false,
+  priority = 100,
+  keys = {
+    { "<leader>sp", "<cmd>Telescope neovim-project discover<cr>", desc = "Project discover" },
+    { "<leader>sP", "<cmd>Telescope neovim-project history<cr>", desc = "Project history" },
+    { "<leader>sQ", "<cmd>NeovimProjectLoadRecent<cr>", desc = "Procejst recent" },
+  },
+}
